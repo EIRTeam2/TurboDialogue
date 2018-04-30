2826f6975796f80535572a4870f56d24cd42cb81
Alex Roman <eirexe123@gmailcom>
2018-04-30 02:20:47 +0200

    Fix typo

--- addons/moe.ero-one.dialog/plugin.gd
+++ addons/moe.ero-one.dialog/plugin.gd
@@ -2,7 +2,7 @@ tool
 extends EditorPlugin
 
 const DIALOG_NODE_SCRIPTS = [
-	preload("res://addons/moe.ero-one.dialog/Nodes/DialogSpeechNode.gd"), 
+	preload("res://addons/moe.ero-one.dialog/Nodes/DialogSpeechNode.gd"),
 	preload("res://addons/moe.ero-one.dialog/Nodes/InputNode.gd"),
 	preload("res://addons/moe.ero-one.dialog/Nodes/RandomChoiceNode.gd")
 ]
@@ -23,34 +23,34 @@ func _enter_tree():
 	dialog_editor = dialog_editor_scene.instance()
 	dialog_editor.plugin = self
 	get_editor_interface().get_editor_viewport().add_child(dialog_editor)
-	
+
 	make_visible(false)
 
 func _exit_tree():
 	remove_custom_type("ERODialogTree")
 	remove_custom_type("ERODialog")
 	dialog_editor.queue_free()
-	
+
 func handles(object):
 	if object is DialogTree:
 		if object.dialog:
 			return true
 	return false
-	
+
 func edit(object):
 	if object is DialogTree:
 		dialog_editor.edit(object.dialog)
-	
+
 func has_main_screen():
 	return true
-	
+
 func make_visible(visible):
 	if dialog_editor:
 		dialog_editor.visible = visible
-		
+
 func get_plugin_name():
-	return "Dialog"
-	
+	return "TurboDialogue"
+
 func get_plugin_icon():
 	return icon
 
@@ -58,4 +58,3 @@ static func get_dialog_node_of_type(type):
 	for node_type in DIALOG_NODE_SCRIPTS:
 		if node_type.get_dialog_node_type() == type:
 			return node_type
-			
